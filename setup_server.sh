976ec3c9b1f4ba5db919af1490cd17cc3850e891
arthurgauffre <arth.gauffre@gmail.com>
2024-04-03 09:30:21 +0200

    [ADD] linode_password, production, redis, base, poll, setup_server.sh, ssh_octopus_key.pub, group_vars/all.yml, postgresql, result, and worker files

---
+++ setup_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y openssh-server python3 python3-pip sudo
+
+export NEW_USER=arthur
+useradd -ms /bin/bash ${NEW_USER}
+echo "${NEW_USER}:azerty" | chpasswd
+mkdir /home/${NEW_USER}/.ssh
+cp /root/.ssh/authorized_keys /home/arthur/.ssh/authorized_keys
+chown -R ${NEW_USER}:${NEW_USER} /home/${NEW_USER}/.ssh
+
+#sudo
+usermod -aG sudo ${NEW_USER}
+echo "${NEW_USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
