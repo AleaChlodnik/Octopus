b2b1fa72997ae5a339c8cf17c33f5c3b6bf9dc93
arthurgauffre <arth.gauffre@gmail.com>
2024-04-04 10:48:52 +0200

    [UPD] installation and configuration scripts and new tar file to delete a folder

--- setup_server.sh
+++ setup_server.sh
@@ -2,11 +2,11 @@
 
 apt-get update && apt-get install -y openssh-server python3 python3-pip sudo
 
-export NEW_USER=arthur
+export NEW_USER=aleachlodnik
 useradd -ms /bin/bash ${NEW_USER}
 echo "${NEW_USER}:azerty" | chpasswd
 mkdir /home/${NEW_USER}/.ssh
-cp /root/.ssh/authorized_keys /home/arthur/.ssh/authorized_keys
+cp /root/.ssh/authorized_keys /home/aleachlodnik/.ssh/authorized_keys
 chown -R ${NEW_USER}:${NEW_USER} /home/${NEW_USER}/.ssh
 
 #sudo
